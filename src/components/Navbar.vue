c8efe2b4119bc876f929ec5df4f2b2b739c33976
faqihakih <faqihakih12@gmail.com>
2020-01-16 01:34:47 +0700

    pusing

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <div class="container">
+        <b-navbar-brand href="#">
+            <b-img src="https://raw.githubusercontent.com/faqihakih/Img/master/plugin.png" fluid alt="Responsive image" width="175" height="50"></b-img>
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">Tim</b-nav-item>
+            <b-nav-item href="#">Tentang</b-nav-item>
+            <b-nav-item href="#">Klien</b-nav-item>
+            <b-nav-item href="#">Produk</b-nav-item>
+            <b-nav-item href="#">Kontak</b-nav-item>
+        </b-navbar-nav>
+        </b-collapse>
+        </div>
+    </b-navbar>
+</div>
+</template>
+
+
+<script lang="ts">
+export default {
+    name: 'Nav',
+    props: {
+        msg : String
+    }
+    
+}
+</script>
+
+<style scoped>
+  .nav-collapse{
+      color: white;
+      font-family:Georgia, 'Times New Roman', Times, serif;
+  }
+</style>
---
+++ src/components/Team.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Team">
+        <div class="">
+            <h1>TEAM</h1>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Team',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+    .Team{
+        background-color: darkslategrey;
+
+    }
+</style>
